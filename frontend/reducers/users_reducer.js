eacf880565e450835581aff0867c9cadf9c36685
your name here <maxjacobkoiwai@gmail.com>
2020-01-05 01:29:43 -0500

    logout issue

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,10 +1,12 @@
-import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { RECEIVE_CURRENT_USER, LOGOUT } from "../actions/session_actions";
 
 export default (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
-      return Object.assign({}, state, { [action.currentUser.id]: action.currentUser })
+      return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+    case LOGOUT:
+      return Object.assign({})
     default:
       return state;
   }
